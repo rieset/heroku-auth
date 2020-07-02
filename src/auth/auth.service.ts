bfa0eecb6961b125d9e0dbf5e21afb3d5cc9c1e0
aiblyaminov <aiblyaminov@wavesplatform.com>
2020-07-02 17:03:50 +0300

    Fix bootstrap version

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,10 +28,13 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { username: user.username, sub: Math.round(Math.random() * 100000)};
-    return {
+    const token = {
       access_token: this.jwtService.sign(payload, {
         expiresIn: new Date(new Date().valueOf() + (120 * 60 * 1000)).valueOf()
       }),
     };
+    console.log('Token', token);
+
+    return token;
   }
 }
